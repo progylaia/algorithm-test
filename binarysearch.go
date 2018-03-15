9daae4110285e2458d445fc523e4e1522d17428f
gaoyu03 <baidu@gaoyudeMacBook-Pro.local>
2018-03-15 23:03:17 +0800

    binarysearch

---
+++ binarysearch.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func binarySearch(arr []int, data int) int {
+	l, r := 0, len(arr)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if arr[mid] == data {
+			return mid
+		}
+		if data > arr[mid] {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return -1
+}
+
+func main()  {
+	testArr := []int{2,3,5,7,9,12}
+	index := binarySearch(testArr, 5)
+	fmt.Printf("%d\n", index)
+}
